b837200f5fa63146b75e26d7aab8d1033f40ba1b
Shojib Mahmud <shojibmahmud109@gmail.com>
2023-06-22 13:49:01 +0600

    Icons implementation

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform_props } from "../hooks/useGames";
+import {
+	FaWindows,
+	FaLinux,
+	FaApple,
+	FaAndroid,
+	FaPlaystation,
+	FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Properties {
+	platforms: Platform_props[];
+}
+
+const PlatformIconList = ({ platforms }: Properties) => {
+	const IconMap: { [key: string]: IconType } = {
+		pc: FaWindows,
+		linux: FaLinux,
+		mac: FaApple,
+		android: FaAndroid,
+		playstation: FaPlaystation,
+		xbox: FaXbox,
+		ios: MdPhoneIphone,
+		nintendo: SiNintendo,
+		web: BsGlobe,
+	};
+	return (
+		<HStack marginY="10px">
+			{platforms.map((platform) => (
+				<Icon color="gray.400" as={IconMap[platform.slug]}></Icon>
+			))}
+		</HStack>
+	);
+};
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClients from "../services/api-clients";
 import { CanceledError } from "axios";
 
+export interface Platform_props {
+	id: number;
+	name: string;
+	slug: string;
+}
+
 export interface Game {
 	id: number;
 	name: string;
 	background_image: string;
+	parent_platforms: { platform: Platform_props }[];
 }
 
 interface FetchGamesResponse {
